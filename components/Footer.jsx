d4b50f69eecb25c8f66d4aeaa549f1033675c248
ybpheno16 <yashubdl@gmail.com>
2024-08-11 00:07:11 +0530

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <p>2022 SonicSells All Rights Reserved.</p>
+      <p className="icons">
+        <AiFillInstagram/>
+        <AiOutlineTwitter/>
+      </p>
+      
+    </div>
+  )
+}
+
+export default Footer;
